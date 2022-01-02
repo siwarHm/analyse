fe37133a79e7768b9091d56e901eec2125623f39
Ayda <96701658+Ayda95@users.noreply.github.com>
2022-01-03 00:37:54 +0100

    Calcul du score d’identité

---
+++ cmake-build-debug/CMakeFiles/analyse.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/analyse.dir/cFiles/maximalLength.c.o"
+  "CMakeFiles/analyse.dir/cFiles/maximalLength.c.o.d"
+  "CMakeFiles/analyse.dir/main.c.o"
+  "CMakeFiles/analyse.dir/main.c.o.d"
+  "analyse"
+  "analyse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/analyse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int transcriptionMain2 () {
     printf("veuillez saisir nom du fichier : \n");
     scanf("%s",filename);
     //definir filePth
-    char path[40] = "/home/projet/Desktop/projet/" ;
+    char path[40] = "/home/projet/Documents/GitHub/analyse/" ;
     //concatenation fileName & filePath
     char *filePath;
     filePath = malloc(strlen(filename)+40);
@@ -82,7 +82,7 @@ int transcriptionMain2 () {
     printf("------------------------------------------------------\n");
     //export du fichier
     FILE *output_transcription;
-    output_transcription = fopen ("/home/projet/Desktop/projet/output_transcription.txt","w+");
+    output_transcription = fopen ("/home/projet/Documents/GitHub/analyse/output_transcription.txt","w+");
     char ligne[82];
     for (int i=0;i< strlen(sequence);i++)
     {
@@ -106,6 +106,147 @@ int transcriptionMain2 () {
 
 
 
+int calculscore () {
+    FILE * fp;
+    FILE * fp2;
+    char sequence[10000] ;
+    char sequence2[10000] ;
+    char codonInitiation[4] = "";
+    int nbLine = 0;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+//recupertion fileName
+    char filename[40];
+    printf("veuillez saisir le nom du premier fichier : \n");
+    scanf("%s",filename);
+//definir filePth
+    char path[40] = "/home/projet/Desktop/projet/" ;
+//concatenation fileName & filePath
+    char *filePath;
+    filePath = malloc(strlen(filename)+40);
+    strcpy(filePath, path);
+    strcat(filePath, filename);
+// ouverture du fichier
+    fp = fopen(filePath , "r");
+    if (fp == NULL){
+        printf("fichier introuvable");
+        calculscore();
+    }
+
+    char filename2[40];
+    printf("veuillez saisir le nom du deuxième fichier : \n");
+    scanf("%s",filename2);
+//concatenation fileName & filePath
+    char *filePath2;
+    filePath2 = malloc(strlen(filename2) + 40);
+    strcpy(filePath2, path);
+    strcat(filePath2, filename2);
+// ouverture du fichier
+    fp2 = fopen(filePath2, "r");
+    if (fp2 == NULL){
+        printf("fichier introuvable");
+        printf("veuillez saisir le nom du deuxième fichier : \n");
+        scanf("%s",filename2);
+    }
+
+    // lecture du fichier ligne par ligne
+    while ((read = getline(&line, &len, fp)) != -1) {
+        //printf("Retrieved line of length %zu:\n", read);
+        //printf("%s", line);
+        nbLine++ ;
+        // verifier codon d'initiation
+        if (nbLine==1){
+            codonInitiation[0] = line[0];
+            codonInitiation[1] = line[1];
+            codonInitiation[2] = line[2];
+            codonInitiation[3] = '\0';
+            int value = strcmp(codonInitiation,"ATG");
+            if (value != 0 ){
+                printf("erreur code d'initiation \n") ;
+                calculscore() ;
+                return 0;
+            }
+        }
+        // concatenation de toutes les lignes
+        char lastLineChar = line[strlen(line)-1] ;
+        if (lastLineChar == '\n'){
+            line[strlen(line)-1] = '\0';
+            strcat(sequence,line);
+        } else {
+            strcat(sequence,line);
+        }
+    }
+    sequence[strlen(sequence)] = '\0';
+    printf(" sequence = %s \n" , sequence);
+
+    fclose(fp);
+
+    while ((read = getline(&line, &len, fp2)) != -1) {
+        //printf("Retrieved line of length %zu:\n", read);
+        //printf("%s", line);
+        nbLine++ ;
+        // verifier codon d'initiation
+        if (nbLine==1){
+            codonInitiation[0] = line[0];
+            codonInitiation[1] = line[1];
+            codonInitiation[2] = line[2];
+            codonInitiation[3] = '\0';
+            int value = strcmp(codonInitiation,"ATG");
+            if (value != 0 ){
+                printf("erreur code d'initiation \n") ;
+                calculscore() ;
+                return 0;
+            }
+        }
+        // concatenation de toutes les lignes
+        char lastLineChar = line[strlen(line)-1] ;
+        if (lastLineChar == '\n'){
+            line[strlen(line)-1] = '\0';
+            strcat(sequence2,line);
+        } else {
+            strcat(sequence2,line);
+        }
+    }
+    sequence2[strlen(sequence2)] = '\0';
+    printf(" sequence2 = %s \n" , sequence2);
+
+    fclose(fp2);
+
+
+    if (line)
+        free(line);
+    // test longeur des deux séquences
+    if (strlen(sequence) != strlen(sequence2) ){
+        printf("Erreur: Les deux séquences ne sont pas égales \n");
+        calculscore();
+        return 0 ;
+    } else{
+        printf("les sequences ont bien la meme longueur \n");
+    }
+    char id[strlen(sequence)];
+
+    double egalite = 0 ;
+        for (int i = 0 ; i < strlen(sequence) ; i++){
+            if (sequence[i] == sequence2[i] ) {
+                id[i] = sequence[i];
+                egalite++;
+            }
+            else {
+                id[i] = '_';
+            }
+        }
+
+
+    printf("-id- : %s \n" , id);
+
+
+
+    double res = egalite  / (double) strlen(sequence) ;
+    printf("Identité de séquence: %0.0f/%lu, soit %.2f %%"  , egalite , strlen(sequence), res * 100 );
+
+
+}
 
 
 
@@ -114,7 +255,8 @@ int transcriptionMain2 () {
 
 int main() {
 
-    transcriptionMain2();
+    calculscore();
+   // transcriptionMain2();
     /*
     int variable ;
     printf("Veuillez choisir la fonctionnalitée : \n");
